adf84fb214e4cc440f80f64abd2a038933d19b5f
iSentrie <xvender7@gmail.com>
2026-02-27 07:56:31 +0200

    feat(add Lithuanian localization messages)

---
+++ locales/lt.lua
@@ -0,0 +1,15 @@
+return {
+    ["noPermission"] = {msg = "Neturite leidimo tai daryti.", type = "error"},
+    ["noSelfIdentifier"] = {msg = "Nepavyko rasti jūsų veikėjo identifikatoriaus.", type = "error"},
+    ["noTargetIdentifier"] = {msg = "Nepavyko rasti veikėjo identifikatoriaus.", type = "error"},
+    ["invalidTargetId"] = {msg = "Neteisingas žaidėjo ID.", type = "error"},
+    ["playerHealed"] = {msg = "Žaidėjas pagydytas.", type = "success"},
+    ["noServerHeal"] = {msg = "Negalite pagydyti serverio.", type = "error"},
+    ["playerStatusFrozen"] = {msg = "Žaidėjas užšaldytas.", type = "success"},
+    ["playerStatusUnfrozen"] = {msg = "Žaidėjas atšaldytas.", type = "success"},
+    ["noServerFreeze"] = {msg = "Negalite sušaldyti serverio.", type = "error"},
+
+    -- Dev command stuff
+    ["invalidAmount"] = {msg = "Neteisingas kiekis.", type = "error"},
+    ["incorrectAction"] = {msg = "Neteisinga veiksmo įvestis.", type = "error"},
+}
