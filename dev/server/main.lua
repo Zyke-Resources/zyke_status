5c8a8cfa42cfd399bb8eb130e57e8875635f3b68
ZykeWasTaken <linussnilsson03@gmail.com>
2026-04-27 18:50:05 +0000

    fix(commands): handle server-ran cmds better

--- dev/server/main.lua
+++ dev/server/main.lua
@@ -1,20 +1,44 @@
 ---@param plyId PlayerId
 local function isAllowed(plyId)
+    if (plyId == 0) then return true end
+
     return Z.hasPermission(plyId, "command")
 end
 
 Z.registerCommand({"status", "stat"}, function(plyId, args)
-    if (not isAllowed(plyId)) then Z.notify(plyId, "noPermission") return end
+    local isInvokerServer = plyId == 0
+
+    if (not isAllowed(plyId)) then
+        if (isInvokerServer) then
+            print("Failed to execute command, reason:", T("noPermission"))
+        else
+            Z.notify(plyId, "noPermission")
+        end
+
+        return
+    end
 
     local primary, secondary, action, amount = args[1], args[2], args[3], tonumber(args[4])
-    if (not amount or type(amount) ~= "number" or amount <= 0.0) then Z.notify(plyId, "invalidAmount") return end
+    if (not amount or type(amount) ~= "number" or amount <= 0.0) then
+        if (isInvokerServer) then
+            print("Failed to execute command, reason:", T("invalidAmount"))
+        else
+            Z.notify(plyId, "invalidAmount")
+        end
+
+        return
+    end
 
     if (action == "add" or action == "+") then
         AddToStatus(plyId, {primary, secondary}, amount)
     elseif (action == "remove" or action == "-") then
         RemoveFromStatus(plyId, {primary, secondary}, amount)
     else
-        Z.notify(plyId, "incorrectAction")
+        if (isInvokerServer) then
+            print("Failed to execute command, reason:", T("incorrectAction"))
+        else
+            Z.notify(plyId, "incorrectAction")
+        end
     end
 end, "Add/Remove from player status", {
     {"primary", "Primary status name (ex. high)"},
@@ -24,9 +48,25 @@ end, "Add/Remove from player status", {
 })
 
 Z.registerCommand({"status_clear", "sclear", "status_reset", "sreset"}, function(plyId, args)
-    if (not isAllowed(plyId)) then Z.notify(plyId, "noPermission") return end
+    local isInvokerServer = plyId == 0
+
+    if (not isAllowed(plyId)) then
+        if (isInvokerServer) then
+            print("Failed to execute command, reason:", T("noPermission"))
+        else
+            Z.notify(plyId, "noPermission")
+        end
+
+        return
+    end
 
     ResetStatuses(plyId)
+
+    if (isInvokerServer) then
+        print("Player statuses have been reset.")
+    else
+        Z.notify(plyId, "statusesReset")
+    end
 end, "Reset Player Status", {
     {"Player Id", "Player Id, or empty to use yourself"}
 })
@@ -34,10 +74,25 @@ end, "Reset Player Status", {
 -- Forcefully save the status of yourself
 -- Mainly used for my own development when restarting & I want to save some initial state to resume from for each script restart
 Z.registerCommand({"status_save"}, function(plyId, args)
-    if (not isAllowed(plyId)) then Z.notify(plyId, "noPermission") return end
+    local isInvokerServer = plyId == 0
+
+    if (not isAllowed(plyId)) then
+        if (isInvokerServer) then
+            print("Failed to execute command, reason:", T("noPermission"))
+        else
+            Z.notify(plyId, "noPermission")
+        end
+
+        return
+    end
 
     SavePlayerToDatabase(plyId)
-    Z.notify(plyId, "statusSaved")
+
+    if (isInvokerServer) then
+        print("Status has been saved.")
+    else
+        Z.notify(plyId, "statusSaved")
+    end
 end)
 
 -- QB eating testing
